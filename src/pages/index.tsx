457d801d9c0e35bade49603cc64587dffc71eacd
juliomchado <juliocarlos00@hotmail.com>
2022-02-22 03:29:18 +0000

    add/ talk to us section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Banner } from "../components/Banner";
 import { Header } from "../components/Header";
 import { Plans } from "../components/Plans";
@@ -27,6 +28,28 @@ function Home() {
                     value={17}
                 />
             </section>
+            <section className={styles.talktous_section}>
+                <div className={styles.talktous_container}>
+                    <div className={styles.talktous_infos_container}>
+                        <h3>Listen from one of the best singers</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. uisque ullamcorper porttitor blandit. Praesent lorem magna, fring</p>
+                        <button>Talk to us</button>
+                    </div>
+                    <div className={styles.talktous_image_container}>
+                        <img src="/images/podcast.png" alt="Night Podcast Peoples Talking" />
+                    </div>
+                </div>
+            </section>
+            {/* <section className={styles.infos}>
+                <div className={styles.}>
+                    <h4>Hear what others say</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. uisque ullamcorper porttitor blandit. Praesent lorem magna, fring</p>
+                    <img src="" alt="" />
+                </div>
+                <div>
+
+                </div>
+            </section> */}
         </>
     )
 
